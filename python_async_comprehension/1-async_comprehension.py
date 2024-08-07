54cf8f03985d152d9583d2588caba08e399b654f
aymelari <aysuabdullayeva25@gmail.com>
2024-08-08 03:33:02 +0400

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,10 @@ from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
-"""The coroutine will collect 10 random numbers using
-    an async comprehensing over async_generator, then
-    return the 10 random numbers."""
-
 
 async def async_comprehension() -> List[float]:
+    """The coroutine will collect 10 random numbers using
+    an async comprehensing over async_generator, then
+    return the 10 random numbers."""
     return [i async for i in async_generator()]
+
