eb138ab91347af6570fe6463372be76626de8ac0
aymelari <aysuabdullayeva25@gmail.com>
2024-08-08 02:03:34 +0400

    task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""
-    Create a measure_time function with integers n and 
+""" Create a measure_time function with integers n and 
     max_delay as arguments that measures the total execution time 
     for wait_n(n, max_delay), and returns total_time / n. 
-    Your function should return a float.
-"""
+    Your function should return a float. """
 import asyncio
 import time
 
