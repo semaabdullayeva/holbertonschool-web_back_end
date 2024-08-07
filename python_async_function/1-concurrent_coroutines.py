d46ccec60b10852ace41362a5b2f80efe19f9914
aymelari <aysuabdullayeva25@gmail.com>
2024-08-08 01:35:20 +0400

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,9 @@
     because of concurrency. """
 import asyncio
 from typing import List
+
 wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     delays: List[float] = []
