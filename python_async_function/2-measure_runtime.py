ac5ec17e8d0678abb5534a0b6254cd68ff1cee21
aymelari <aysuabdullayeva25@gmail.com>
2024-08-08 01:54:34 +0400

    task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,6 +2,7 @@
 """
 Create a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n. Your function should return a float.
 """
+import asyncio
 import time
 
 wain_n = __import__("1-concurrent_coroutine").wait_n
