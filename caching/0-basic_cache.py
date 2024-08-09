984f5737cf3eae43c16d47da1b1306fd834d2cff
semaabdullayeva <semaabdullayeva02@gmail.com>
2024-08-09 18:31:55 +0400

    task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,15 +2,16 @@
 """ Basic dictionary
 """
 from base_caching import BaseCaching
+
+
 class BasicCache(BaseCaching):
     def put(self, key, item):
-        """ Assign to the dictionary """
+        """Assign to the dictionary"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Return the value linked """
+        """Return the value linked"""
         if key is None or self.cache_data.get(key) is None:
             return None
         return self.cache_data.get(key)
-    
