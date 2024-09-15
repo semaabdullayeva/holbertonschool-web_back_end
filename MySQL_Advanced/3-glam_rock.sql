11f376f445cd70664e1d18ded89a201cf1c8814d
SemaAbdullayeva <semaabdullayeva02@gmail.com>
2024-09-15 22:49:55 +0400

    update

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
--- SQL script that lists all bands with Glam rock as their main style
--- Script that performs task
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+-- Search bands with style Glam rock
+-- Calculate duration based on the current year if split is NULL
+SELECT band_name,
+       IFNULL(split, YEAR(CURDATE())) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
