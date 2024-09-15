c641c6d8bf47ad8efd8fc7d9dbb69a895248a6a8
SemaAbdullayeva <semaabdullayeva02@gmail.com>
2024-09-15 17:44:55 +0400

    update

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- SQL script that creates a table users
--- Table creation script
+-- Create table users if it doesn't already exist
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
